8a907e38b84ce94be277ca47fbb089889a96369f
violethaze74 <65783327+violethaze74@users.noreply.github.com>
2021-10-23 20:46:29 -0400

    Initial commit

---
+++ pkg/cmd/codespace/root.go
@@ -0,0 +1,34 @@
+package codespace
+
+import (
+	"github.com/MakeNowJust/heredoc"
+	"github.com/spf13/cobra"
+)
+
+func NewRootCmd(app *App) *cobra.Command {
+	root := &cobra.Command{
+		Use:           "codespace",
+		SilenceUsage:  true,  // don't print usage message after each error (see #80)
+		SilenceErrors: false, // print errors automatically so that main need not
+		Short:         "List, create, delete and SSH into codespaces",
+		Long:          `Work with GitHub codespaces`,
+		Example: heredoc.Doc(`
+			$ gh codespace list
+			$ gh codespace create
+			$ gh codespace delete
+			$ gh codespace ssh
+		`),
+	}
+
+	root.AddCommand(newCodeCmd(app))
+	root.AddCommand(newCreateCmd(app))
+	root.AddCommand(newDeleteCmd(app))
+	root.AddCommand(newListCmd(app))
+	root.AddCommand(newLogsCmd(app))
+	root.AddCommand(newPortsCmd(app))
+	root.AddCommand(newSSHCmd(app))
+	root.AddCommand(newCpCmd(app))
+	root.AddCommand(newStopCmd(app))
+
+	return root
+}
